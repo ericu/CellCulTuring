319f38f26a0e3f2dcd481a48914d9c83549fc651
Eric Uhrhane <eric@elemenohealth.com>
2018-11-10 08:29:26 -0800

    Init for bounce.

--- bounce.js
+++ bounce.js
@@ -5,11 +5,11 @@
  // Ball squish involves a compression layer counter.
  // Hard-code ball as 3x3 for now?  Nah--start with 1x1 and add compression
  // later.  So just ball [with velocity], background, wall.
- const C_WALL = 0xff000088;
- const C_BACKGROUND = 0xff880000;
- const C_BALL = 0xff88ff88;
+ const C_WALL = 0xff0000ff;
+ const C_BACKGROUND = 0xffff0000;
+ const C_BALL = 0xff00ff00;
 
- function fillStyleFromUint(u) {
+ function styleFromUint(u) {
    let a = u >>> 24 & 0xff;
    let r = u >>> 16 & 0xff;
    let g = u >>>  8 & 0xff;
@@ -20,14 +20,18 @@
  function initBounce(canvas) {
    let context = canvas.getContext('2d');
 
-   context.fillStyle = fillStyleFromUint(C_BACKGROUND);
+
+   context.fillStyle = styleFromUint(C_BACKGROUND);
    context.fillRect(0, 0, canvas.width, canvas.height);
 
-   context.fillStyle = fillStyleFromUint(C_WALL);
-   context.strokeRect(0, 0, canvas.width, canvas.height);
+   // Strokes are between lines, so they end up fuzzy; fills aren't.
+   context.translate(0.5, 0.5);
+   context.strokeStyle = styleFromUint(C_WALL);
+   context.strokeRect(0, 0, canvas.width - 1, canvas.height - 1);
+   context.translate(-0.5, -0.5);
 
-   context.fillStyle = fillStyleFromUint(C_BALL);
-   context.fillRect(canvas.width / 2, canvas.height / 2, 3, 3);
+   context.fillStyle = styleFromUint(C_BALL);
+   context.fillRect(canvas.width / 2, canvas.height / 2, 1, 1);
  }
 
  function bounce(data) {
