7e3c76e0b0c14bb0c2ce8602444245b098d727f4
Eric Uhrhane <eric@elemenohealth.com>
2018-11-11 07:36:50 -0800

    Untested implementation of MotionState.

--- anglebounce.js
+++ anglebounce.js
@@ -18,97 +18,17 @@
     bm.declare('BALL_FLAG', 2, 14); // Could use 1 bit, but it's rather dim.
     bm.declare('FULL_ALPHA', 8, 24);
 
-    // TODO: Use a 5-bit encoding that covers the full 7x7 surrounding square,
-    // for 32 real directions.  Then we'll need 2 more bits for Bresenham state.
-    // The first two bits pick the quadrant, and then next 3 use a lookup table
-    // to get their specifics.  Could allocate one of those 3 to whether x or y
-    // is primary, which may be convenient, but it's still not symmetric after
-    // that.  Here the highest bit indicates Y primary if set.
-    //  .   .   .   .   7   5   .
-    //  .   .   .   .   6   .   3
-    //  .   .   .   .   4   2   1
-    //  .   .   .   x   0   .   .
-    //  .   .   .   .   .   .   .
-    //  .   .   .   .   .   .   .
-    //  .   .   .   .   .   .   .
+    bm.declare('MOVE_R_NOT_L', 1, 8); // In ball color for now.
+    bm.declare('MOVE_D_NOT_U', 1, 9); // In ball color for now.
+    bm.declare('MOVE_STATE', 2, 10);
+    bm.declare('MOVE_INDEX', 4, 0); // In the bottom of alpha for now.
 
-    // From the bits, we'll want a 'given this color, where does the ball want
-    // to be next cycle', which takes into account the Bresenham state.  Then
-    // we'll need a 'given this current state, produce the next color', which
-    // updates the state before setting the color.  We'll also need a "bounce
-    // this state off the x or y axis" which should also adjust the Bresenham
-    // state accordingly [however we arbitrarily decide it should get
-    // adjusted].
-
-    // Given the above encoding, to support the full range of motions, the
-    // quadrant selection needs to be a rotation.  However, since we'll never go
-    // straight up or down in pong, we can do reflections instead, which is a
-    // lot easier, and we'll just have redundant encodings for straight
-    // horizontal.
-
-    // The lookup table should produce [bresenhamMax, bresenhamIncrement,
-    // primaryMotion].
-
-    const motionTable = [
-      {
-        dir = 'x';
-        bInc = 0;
-        bMax = 0;
-      },
-      {
-        dir = 'x';
-        bInc = 1;
-        bMax = 3;
-      },
-      {
-        dir = 'x';
-        bInc = 1;
-        bMax = 2;
-      },
-      {
-        dir = 'x';
-        bInc = 2;
-        bMax = 3;
-      },
-      {
-        dir = 'y'; // whichever
-        bInc = 1;
-        bMax = 1;
-      },
-      {
-        dir = 'y';
-        bInc = 2;
-        bMax = 3;
-      },
-      {
-        dir = 'y';
-        bInc = 1;
-        bMax = 2;
-      },
-      {
-        dir = 'y';
-        bInc = 1;
-        bMax = 3;
-      },
-    ]
-    // To be written:
-    // ballMotionStateFromColor() [produces a BallMotionState object],
-    // ballMotionFromState() [takes BallMotionState, produces offset]
-    // reflectBallMotion(axis) [just flips one of the 2 high bits]
-    // nextColorFromBallMotion() [increments Bresenham state]...and some way of
-    // telling if the ballMotion hits the current cell, so
-    // sourceDirectionOffsetFromIndex [produces offset from index].
-
-    // Ball motion bits; for now, put them in ball color low bits, but they
-    // could also go in alpha low bits.
-    bm.declare('C_MOVE_R_NOT_L', 1, 8);
-    bm.declare('C_MOVE_D_NOT_U', 1, 9);
-
-    bm.combine('BALL_MOTION_TEMP_HACK', ['C_MOVE_R_NOT_L', 'C_MOVE_D_NOT_U']);
+    bm.combine('BALL_MOTION_TEMP_HACK', ['MOVE_R_NOT_L', 'MOVE_D_NOT_U']);
     bm.combine('C_WALL', ['FULL_ALPHA', 'WALL_FLAG']);
     bm.alias('C_BACKGROUND', 'FULL_ALPHA');
     bm.combine('C_BALL', ['FULL_ALPHA', 'BALL_FLAG']);
   }
+
 
   function isWall (c) {
     return bm.isSet('WALL_FLAG', c);
@@ -136,8 +56,8 @@
 
   // For now, just do the 4 45-degree angles, nothing else.
   function ballDirectionFromColor(color) {
-    let vX = bm.get('C_MOVE_R_NOT_L', color) ? 1 : -1;
-    let vY = bm.get('C_MOVE_D_NOT_U', color) ? 1 : -1;
+    let vX = bm.get('MOVE_R_NOT_L', color) ? 1 : -1;
+    let vY = bm.get('MOVE_D_NOT_U', color) ? 1 : -1;
     return { vX: vX, vY: vY }
   }
 
@@ -145,8 +65,8 @@
     let positiveX = dir.vX > 0 ? 1 : 0;
     let positiveY = dir.vY > 0 ? 1 : 0;
     let color = bm.getMask('C_BALL')
-    color = bm.set('C_MOVE_R_NOT_L', color, positiveX)
-    color = bm.set('C_MOVE_D_NOT_U', color, positiveY)
+    color = bm.set('MOVE_R_NOT_L', color, positiveX)
+    color = bm.set('MOVE_D_NOT_U', color, positiveY)
     return color;
   }
 
@@ -182,8 +102,8 @@
         break;
       default: assert(false);
     }
-    let packed = bm.set('C_MOVE_R_NOT_L', 0, dirBits[0] > 0);
-    packed = bm.set('C_MOVE_D_NOT_U', packed, dirBits[1] > 0);
+    let packed = bm.set('MOVE_R_NOT_L', 0, dirBits[0] > 0);
+    packed = bm.set('MOVE_D_NOT_U', packed, dirBits[1] > 0);
     return bm.get('BALL_MOTION_TEMP_HACK', packed);
   }
 
---
+++ motionstate.js
@@ -0,0 +1,128 @@
+// We use a 5-bit encoding that covers the full 7x7 surrounding square,
+// for 32 real directions.  Then we'll need 2 more bits for Bresenham state.
+// The first two bits of the five pick the quadrant, and then next 3 use a
+// lookup table to get their specifics.  Could allocate one of those 3 to
+// whether x or y is primary, which may be convenient, but it's still not
+// symmetric after that.  Here the highest bit indicates Y primary if set.
+//  .   .   .   .   7   5   .
+//  .   .   .   .   6   .   3
+//  .   .   .   .   4   2   1
+//  .   .   .   x   0   .   .
+//  .   .   .   .   .   .   .
+//  .   .   .   .   .   .   .
+//  .   .   .   .   .   .   .
+
+// Given the above encoding, to support the full range of motions, the
+// quadrant selection needs to be a rotation.  However, since we'll never go
+// straight up or down in pong, we can do reflections instead, which is a
+// lot simpler for bounces, and we'll just have redundant encodings for straight
+// horizontal.
+
+(function () {
+
+const motionTable = [
+  {
+    dir = 'x';
+    bInc = 0;
+    bMax = 0;
+  },
+  {
+    dir = 'x';
+    bInc = 1;
+    bMax = 3;
+  },
+  {
+    dir = 'x';
+    bInc = 1;
+    bMax = 2;
+  },
+  {
+    dir = 'x';
+    bInc = 2;
+    bMax = 3;
+  },
+  {
+    dir = 'y'; // whichever
+    bInc = 1;
+    bMax = 1;
+  },
+  {
+    dir = 'y';
+    bInc = 2;
+    bMax = 3;
+  },
+  {
+    dir = 'y';
+    bInc = 1;
+    bMax = 2;
+  },
+  {
+    dir = 'y';
+    bInc = 1;
+    bMax = 3;
+  },
+]
+
+class MotionState {
+  constructor(color) {
+//    assert(isBall(this.color);
+    this.color = color;
+    this.right = bm.get('MOVE_R_NOT_L', color);
+    this.down = bm.get('MOVE_D_NOT_U', color);
+    this.state = bm.get('MOVE_STATE', color);
+    this.index = bm.get('MOVE_INDEX', color);
+    assert(this.index >= 0 && this.index < motionTable.length);
+
+    let dX = 0, dY = 0;
+    let record = motionTable[this.index];
+
+    let nextState = this.state + record.bInc;
+    let overflow = false;
+    if (nextState > record.bMax) {
+      overflow = true;
+      nextState -= record.bMax;
+    }
+    if (record.dir == 'x') {
+      dX = 1;
+      if (overflow) {
+        dY = 1;
+      }
+    } else {
+      dY = 1;
+      if (overflow) {
+        dX = 1;
+      }
+    }
+    if (!this.right) {
+      dX = -dX;
+    }
+    if (!this.down) {
+      dY = -dY;
+    }
+    this.dX = dX;
+    this.dY = dY;
+    this.nextState = nextState;
+  }
+
+  reflect(axis) {
+    if (axis === 'x') {
+      this.right = !this.right;
+    }
+    else if (axis === 'y') {
+      this.down = !this.down;
+    } else {
+      assert(false);
+    }
+  }
+
+  nextColor() {
+    let color = this.color;
+    color = bm.set('MOVE_R_NOT_L', color, this.right);
+    color = bm.set('MOVE_D_NOT_U', color, this.down);
+    color = bm.set('MOVE_STATE', color, this.nextState);
+    return color;
+  }
+}
+
+window.MotionState = MotionState
+})();
