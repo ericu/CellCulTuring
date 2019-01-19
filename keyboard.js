6020ab83c67b9ddce3ba6443b4a8c116aaee0483
Eric Uhrhane <2245151+ericu@users.noreply.github.com>
2019-01-19 13:59:55 -0800

    Add missing file.

---
+++ keyboard.js
@@ -0,0 +1,17 @@
+(function () {
+  window.keyTable = {}
+  window.addEventListener('keydown', onKeyDown)
+  window.addEventListener('keyup', onKeyUp)
+
+  function onKeyDown(event) {
+    let key = event.key.toLowerCase()
+    if (!keyTable[key]) {
+      keyTable[key] = true;
+    }
+  }
+
+  function onKeyUp(event) {
+    let key = event.key.toLowerCase()
+    keyTable[key] = false;
+  }
+})()
